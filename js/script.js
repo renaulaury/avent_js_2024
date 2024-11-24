be814adc5d7cf034152d7fba11d505c06962a196
Lily <112404773+Belywen@users.noreply.github.com>
2024-11-24 17:01:36 +0100

    maj avent

--- js/script.js
+++ js/script.js
@@ -2,16 +2,12 @@ const section = document.querySelector('section');
 const cases = document.createElement('div');
 cases.classList.add('case');
 
-// const colors = [
-//     '#8B5F33', '#E0A96D', '#7C4E2F', '#B97B2F', '#C78203', '#9A6E3B',
-//     '#D89B5A', '#936F56', '#8D6E4B', '#A56E3C', '#D89B5A', '#7A513C',
-//     '#B56E31', '#E0A96D', '#8B5F33', '#9A6E3B', '#C78203', '#7C4E2F',
-//     '#B97B2F', '#A56E3C', '#936F56', '#D89B5A', '#8D6E4B', '#7A513C'
-// ]; 
-
-const numero = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 
-                11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
-                21, 22, 23, 24]
+const colors = [
+    '#8B5F33', '#E0A96D', '#7C4E2F', '#B97B2F', '#C78203', '#9A6E3B',
+    '#D89B5A', '#936F56', '#8D6E4B', '#A56E3C', '#D89B5A', '#7A513C',
+    '#B56E31', '#E0A96D', '#8B5F33', '#9A6E3B', '#C78203', '#7C4E2F',
+    '#B97B2F', '#A56E3C', '#936F56', '#D89B5A', '#8D6E4B', '#7A513C'
+]; 
 
 
 function afficherCalendrier() {   
@@ -21,7 +17,8 @@ function afficherCalendrier() {
         newCase.innerText = i;
         section.appendChild(newCase);        
     }
-    shuffleCases()
+    shuffleCases();
+    shuffleColors();
 }
 
 function shuffleCases() { 
@@ -29,7 +26,7 @@ function shuffleCases() {
     Obligée de créer les élément avec for
     Puis de les récupérer avec Array.from(section.children)*/
 
-    let casesShuffle = Array.from(section.children); 
+    let casesShuffle = Array.from(section.children); //new tableau de new numéros
     let i = casesShuffle.length, k, temp;
     
     while(--i > 0) {
@@ -41,6 +38,25 @@ function shuffleCases() {
 
     for (let i = 0; i < casesShuffle.length; i++) {
         section.appendChild(casesShuffle[i]); //Je met les new cases dans la section
+    }
+}
+
+function shuffleColors() { 
+    /*ici je peux utiliser mon tableau directement*/
+
+    let i = colors.length, k, temp;
+    
+    while(--i > 0) {
+        k = Math.floor(Math.random() * (i+1));
+        temp = colors[k];
+        colors[k] = colors[i];
+        colors[i] = (temp); 
+    }
+
+    let colorsShuffle = Array.from(section.children);//new tableau de new colors
+    
+    for (let i = 0; i < colorsShuffle.length; i++) {
+        colorsShuffle[i].style.backgroundColor = colors[i]; // Je réattribue les new colors
     }
 }
 
